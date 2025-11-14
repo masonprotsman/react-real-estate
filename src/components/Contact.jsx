7d5f525f1e7dd42c7091512270c0a992688990e6
masonprotsman <masonprotsman@gmail.com>
2025-11-14 14:08:38 -0500

    access key

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         setResult("Sending....");
         const formData = new FormData(event.target);
 
-        formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+        formData.append("access_key", "7add464b-ab80-4010-9966-748c13537cca");
 
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
